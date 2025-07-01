2222e734adc7846506bc5deaa6b0f3dd71f0ca7e
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-07-01 17:17:55 -0300

    feat: create user controller with validation and bcrypt

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+
 import { hash } from "bcrypt";
 import { z } from "zod";
 
@@ -12,9 +15,25 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body);
 
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
     const hashedPassword = await hash(password, 8);
 
-    return response.json({ message: "ok", hashedPassword });
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
